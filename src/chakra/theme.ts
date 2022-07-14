25e90f766804cac0ccab2708ab12daa9eabd2c22
putraperdana138 <80741806+putraprdn@users.noreply.github.com>
2022-07-14 10:05:58 +0700

    feat(component): create search & button components

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -2,6 +2,7 @@ import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
 import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./button";
 
 export const theme = extendTheme({
 	colors: {
@@ -9,10 +10,10 @@ export const theme = extendTheme({
 			100: "#FF3C00",
 		},
 	},
-	font: {
+	fonts: {
 		body: "Open Sans, sans-serif",
 	},
-	style: {
+	styles: {
 		global: () => ({
 			body: {
 				bg: "gray.200",
@@ -20,6 +21,6 @@ export const theme = extendTheme({
 		}),
 	},
 	components: {
-		// Button
+		Button,
 	},
 });
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,23 @@
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
-	return <div>Navbar</div>;
+	return (
+		<Flex bg="white" height="44px" padding="6px 12px">
+			<Flex align={"center"}>
+				<Image src="/images/redditFace.svg" height="13px" />
+				<Image
+					src="/images/redditText.svg"
+					height="46px"
+					display={{ base: "none", md: "unset" }}
+				/>
+			</Flex>
+			{/* <Directory /> */}
+			<SearchInput />
+			<RightContent />
+		</Flex>
+	);
 };
 export default Navbar;
